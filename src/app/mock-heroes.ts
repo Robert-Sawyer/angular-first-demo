c80982ad1a95e9f544054b79b884fb47fe49f484
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-08-12 12:41:55 +0200

    Dodanie listy bohaterów

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Falcon'},
+  {id: 12, name: 'Hulk'},
+  {id: 13, name: 'Hawkeye'},
+  {id: 14, name: 'Quicksilver'},
+  {id: 15, name: 'Magneto'}
+];
+
